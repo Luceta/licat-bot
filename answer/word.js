0067ba04da49f989e924cf372c43ddba5ee42cf3
Luceta <57941164+Luceta@users.noreply.github.com>
2022-02-05 21:29:28 +0900

    Refactor: word.js & wordlist

--- answer/word.js
+++ answer/word.js
@@ -1,47 +1,53 @@
 const Discord = require("discord.js");
 
+const database = [
+  {
+    wordArr: ["신나", "좋아", "최고"],
+    title: "무야~ 호~",
+    image: "https://dimg.donga.com/wps/NEWS/IMAGE/2021/11/12/110217903.2.jpg",
+  },
+
+  {
+    wordArr: ["오 마이갓", "킹", "omg"],
+    title: "",
+    image: "https://thumbs.gfycat.com/YellowishNarrowFoal-size_restricted.gif",
+  },
+
+  {
+    wordArr: ["멈춰"],
+    title: "Stop!",
+    image: "https://c.tenor.com/BUFmvH6RjgUAAAAM/%EB%A9%88%EC%B6%B0-stop.gif",
+  },
+
+  {
+    wordArr: ["커피"],
+    title: "",
+    image:
+      "https://cdn.discordapp.com/attachments/903526029077929984/909098394780315718/zzal.gif",
+  },
+];
+
 const answerFunnyWord = (msg) => {
-  const check = msg.content.includes("신나") || msg.content.includes("좋아");
-  const lifeOfTheParty = msg.content.includes("제섭");
-  const omg =
-    msg.content.includes("오 마이갓") ||
-    msg.content.includes("킹") ||
-    msg.content.includes("omg");
-  const stop = msg.content.includes("멈춰");
+  const sendMessage = (msg, { wordArr, title, image }) => {
+    const check = wordArr.some((word) => {
+      return msg.content.includes(word);
+    });
 
-  if (check) {
-    const embed = new Discord.MessageEmbed()
-      .setTitle("무야~ 호~")
-      .setImage(
-        "https://dimg.donga.com/wps/NEWS/IMAGE/2021/11/12/110217903.2.jpg"
-      );
-    embed.setTitle("무야~ 호~");
-    embed.setImage(
-      "https://dimg.donga.com/wps/NEWS/IMAGE/2021/11/12/110217903.2.jpg"
-    );
-    return msg.channel.send(embed);
-  }
-  if (lifeOfTheParty) {
-    const embed = new Discord.MessageEmbed()
-      .setTitle("나는 야 핵인싸 홍제섭~")
-      .setImage("https://c.tenor.com/Utfm0NszTAcAAAAM/%EA%B5%B0%EC%B9%A8.gif")
-      .setImage("https://c.tenor.com/Utfm0NszTAcAAAAM/%EA%B5%B0%EC%B9%A8.gif");
-    return msg.channel.send(embed);
-  }
+    if (check) {
+      const embed = new Discord.MessageEmbed().setTitle(title).setImage(image);
+      msg.channel.send(embed);
+    }
+  };
 
-  if (omg) {
-    const embed = new Discord.MessageEmbed().setImage(
-      "https://thumbs.gfycat.com/YellowishNarrowFoal-size_restricted.gif"
-    );
-    return msg.channel.send(embed);
-  }
+  database.forEach((doc) => sendMessage(msg, doc));
+};
 
-  if (stop) {
-    const embed = new Discord.MessageEmbed().setImage(
-      "https://c.tenor.com/BUFmvH6RjgUAAAAM/%EB%A9%88%EC%B6%B0-stop.gif"
-    );
-    return msg.channel.send(embed);
-  }
+const answerFunnyDb = () => {
+  const wordList = [];
+  database.forEach(({ wordArr }) => wordList.push(wordArr.join("/")));
+
+  return `[라이캣이 반응하는 단어 목록]\n\n${wordList.join(", ")}`;
 };
 
 exports.answerFunnyWord = answerFunnyWord;
+exports.answerFunnyDb = answerFunnyDb;
